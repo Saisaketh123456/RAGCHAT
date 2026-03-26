017a9023a1fcd640125ce51c994b1bfe3f280850
Saisaketh123456 <ssaketh3062002@gmail.com>
2026-03-26 16:01:22 +0530

    Updated 3 files

--- LANCHAT.py
+++ LANCHAT.py
@@ -20,7 +20,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_community.retrievers import BM25Retriever
-from langchain.retrievers.ensemble import EnsembleRetriever
+from langchain.retrievers import EnsembleRetriever
 
 from langchain.schema import Document
 from langchain_groq import ChatGroq
